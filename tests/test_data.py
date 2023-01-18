cdc87f0bc7957f99a66b823a1efdab6b117611a4
Rune-Nedergaard <rune7863@gmail.com>
2023-01-18 16:10:38 +0100

    test works

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,16 +6,19 @@ import torch
 
 from tests import _PATH_DATA
 
-
+#_TEST_ROOT = os.path.dirname(__file__)  # root of test folder
+#_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)  # root of project
+#_PATH_DATA = os.path.join(_PROJECT_ROOT, "data")  # root of data
 
 def test_data():
-    train_image_path = f"{_PATH_DATA}/processed/train.py"
+    train_image_path = f"{_PATH_DATA}/processed/train.pt"
 
-    test_image_path = f"{_PATH_DATA}/processed/test.py"
+    test_image_path = f"{_PATH_DATA}/processed/test.pt"
 
     N_train = 25000
     N_test = 5000
     train_images = torch.load(train_image_path)
     test_images = torch.load(test_image_path)
-    assert len(train_images) == N_train
-    assert len(test_images) == N_test
+    assert len(train_images[0]) == N_train
+    assert len(test_images[0]) == N_test
+
